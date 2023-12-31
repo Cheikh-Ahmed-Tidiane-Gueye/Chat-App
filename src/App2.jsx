984a68ccf5d411a69fa3842e853ed217befcf859
Cheikh Ahmed Tidiane Gueye <cheikhahmedtidiane219@gmail.com>
2023-12-31 20:26:45 +0000

    test

--- src/App2.jsx
+++ src/App2.jsx
@@ -6,6 +6,7 @@ import {
   useNavigate,
   Link
 } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 import "./App.css";
 import "./style.scss";
@@ -16,6 +17,8 @@ import Connexion from "./pages/Connexion";
 import Inscription from "./pages/Inscription";
 
 export default function App() {
+
+  const [theme, setTheme] = useState('light')
    
   const rooter = createBrowserRouter([
     {
@@ -32,5 +35,11 @@ export default function App() {
     },
   ]);
 
-  return <RouterProvider router={rooter} />;
+  return (
+    <>
+      <AuthContext.Provider value={""}>
+        <RouterProvider router={rooter} />;
+      </AuthContext.Provider>
+    </>
+  );
 }
